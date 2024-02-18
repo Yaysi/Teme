cfc5ffa0dd1b8d51d2eff2b42a0c6acfb4733256
Ismail <ismailtikhlal74@gmail.com>
2024-02-18 17:18:02 +0200

    Hello

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
+        System.out.println("Ismail");
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(100);
+
     }
 }
